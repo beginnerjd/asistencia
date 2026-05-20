12ae89174ea26614c28c07cd8c1d71d91b5f9073
Beginnerjd <juan.diego.ramos@correounivalle.edu.co>
2026-05-19 19:45:54 -0500

    Change API_URL to new local IP address
    Updated API_URL to new local IP address

--- app/src/config/api.js
+++ app/src/config/api.js
@@ -1,5 +1,2 @@
-// IMPORTANTE:
-// Cambia esta IP por la IPv4 de tu computador.
-// Ejemplo: http://192.168.1.15:3001
-// El celular y el PC deben estar conectados a la misma red WiFi.
+
 export const API_URL = 'http://192.168.101.9:3001';
